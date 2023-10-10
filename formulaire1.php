673fbcb67628fcc5e427bd180283140a51c967f6
nix202 <earcanix@gmail.cm>
2023-10-10 14:43:49 -0400

    registration form

---
+++ formulaire1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <body>
+<form method="post" action="resultat.php">
+<h1> REGISTRATION </h1>
+
+<label> username: </label>
+<input type="text" username="user" id="test" ><br>
+<label> passeword: </label>
+<input type="text"passeword="passeword" id="test" ><br>
+<input type="submit">
+
+
+</form>
+    </body><!DOCTYPE html>
+
+</html>
---
+++ formulaire2.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <body>
+<form method="post" action="resultat.php">
+<h1> PASSEWORD CONFIMATION</h1>
+
+
+<label> comfirm_passeword: </label>
+<input type="text"comfirm_passeword="passeword" id="test" ><br>
+<input type="submit">
+
+
+</form>
+    </body><!DOCTYPE html>
+
+</html>
